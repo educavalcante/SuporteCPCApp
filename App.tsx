88f3dbefd8881f2ed91f839d869552010124dd18
educavalcante <eduardo@cpcbrasil.com>
2023-04-20 00:10:48 -0300

    navigation, stack navigator, tipagem de telas, navegacao rotas de autenticacao

--- App.tsx
+++ App.tsx
@@ -2,17 +2,16 @@
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
 
-import { Loading } from '@components/Loading';
-import { SignIn } from './src/screens/Signin';
-
+import { Routes } from './src/routes';
 
 import { THEME } from './src/theme'
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
   return (
     <NativeBaseProvider theme={THEME}>
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
---
+++ src/screens/SignUp.tsx
@@ -0,0 +1,43 @@
+import { VStack, Text, Center, Heading, View, ScrollView } from 'native-base';
+
+import LogoSvg from '@assets/logomarcacpcbotom.svg';
+import MarcaSvg from '@assets/marca.svg';
+
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
+
+export function SignIn() {
+    return (
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+            <VStack flex={1} bg="gray.400" px={10}>
+                <Center my={20}>
+                    <MarcaSvg />
+                </Center>
+                <View bg="gray.100" borderRadius={15} px={6} >
+                    <Center>
+                        <Heading color="gray.700" mt={6} fontSize="lg" mb={6} fontFamily="heading">
+                            Entrar
+                        </Heading>
+                        <Input
+                            placeholder="Nome"
+                        />
+                        <Input
+                            placeholder="E-mail"
+                            keyboardType="email-address"
+                            autoCapitalize='none' //manter texto em minusculo
+                        />
+                        <Input
+                            placeholder="Senha"
+                            secureTextEntry  //nao mosrar o texto da senha 
+                        />
+                        <Text color="gray.700" fontSize="8" mb={10}>
+                            Não tem senha?
+                        </Text>
+                        <Button title="Criar acesso" mb={10} />
+
+                    </Center>
+                </View>
+            </VStack>
+        </ScrollView>
+    );
+}
