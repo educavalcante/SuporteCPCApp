ba07dd46c20bb4df5f50be20a946220fcdaaf60a
educavalcante <eduardo@cpcbrasil.com>
2023-05-06 01:29:26 -0300

    componente grupo, estilizacao do botão Pressable, flatlist para montagem dos botões

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,32 @@
+import { useState } from 'react';
 import { HomeHeader } from '@components/HomeHeader';
-import { Center, Text, VStack } from 'native-base';
+import { Group } from '@components/Group';
+import { Center, FlatList, Text, VStack } from 'native-base';
 
 export function Home() {
+    const [groups, setgroups] = useState(["tarefas", "agenda", "chamados", "Rats"]) 
+    const [groupSelected, setGrouSelected] = useState('tarefas');
+
     return (
         <VStack flex={1}>
            <HomeHeader/>
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({item})=>(
+                    <Group 
+                    name={item}
+                    isActive={groupSelected === item} 
+                    onPress={()=> setGrouSelected(item)}
+                    />
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                _contentContainerStyle={{ px: 6 }}   
+                my={10}
+                maxH={10}
+            />
+
         </VStack>
     )
 }
